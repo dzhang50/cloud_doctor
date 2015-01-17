c8de218265f5273e60fad6f94f3a4f209d365982
Peter Kim <peterkim45366@gmail.com>
2015-01-17 13:10:05 -0500

    installed coreplot

--- clouddoctorapp/Pods/Target Support Files/Pods-clouddoctorapp/Pods-clouddoctorapp-environment.h
+++ clouddoctorapp/Pods/Target Support Files/Pods-clouddoctorapp/Pods-clouddoctorapp-environment.h
@@ -60,6 +60,12 @@
 #define COCOAPODS_VERSION_MINOR_CWStatusBarNotification 2
 #define COCOAPODS_VERSION_PATCH_CWStatusBarNotification 2
 
+// CorePlot
+#define COCOAPODS_POD_AVAILABLE_CorePlot
+#define COCOAPODS_VERSION_MAJOR_CorePlot 1
+#define COCOAPODS_VERSION_MINOR_CorePlot 5
+#define COCOAPODS_VERSION_PATCH_CorePlot 1
+
 // Parse
 #define COCOAPODS_POD_AVAILABLE_Parse
 #define COCOAPODS_VERSION_MAJOR_Parse 1
