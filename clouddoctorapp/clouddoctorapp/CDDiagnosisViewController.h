fcfdd528cc3633ba5383fde1ae385a6273ad1a37
Peter Kim <peterkim45366@gmail.com>
2015-01-17 07:09:53 -0500

    added diagnosis view controller

---
+++ clouddoctorapp/clouddoctorapp/CDDiagnosisViewController.h
@@ -0,0 +1,13 @@
+//
+//  CDDiagnosisViewController.h
+//  clouddoctorapp
+//
+//  Created by Peter Kim on 1/17/15.
+//  Copyright (c) 2015 Cloud Doctor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CDDiagnosisViewController : UIViewController
+
+@end
---
+++ clouddoctorapp/clouddoctorapp/CDDiagnosisViewController.m
@@ -0,0 +1,37 @@
+//
+//  CDDiagnosisViewController.m
+//  clouddoctorapp
+//
+//  Created by Peter Kim on 1/17/15.
+//  Copyright (c) 2015 Cloud Doctor. All rights reserved.
+//
+
+#import "CDDiagnosisViewController.h"
+
+@interface CDDiagnosisViewController ()
+
+@end
+
+@implementation CDDiagnosisViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
